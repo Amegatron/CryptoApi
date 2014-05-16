20b104272c58b19f4517f7e90113e84eef99608e
Amegatron <amego2006@gmail.com>
2014-05-16 21:18:16 +0400

    First Commit

---
+++ src/Amegatron/Cryptoapi/CryptoapiServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Amegatron\Cryptoapi;
+
+use Illuminate\Support\ServiceProvider;
+
+class CryptoapiServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('amegatron/cryptoapi');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
